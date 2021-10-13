dd468a9bc01449befa7ff299ff73da536556acfe
Youssef Oufkir <75744745+Youssef-O@users.noreply.github.com>
2021-10-13 14:42:14 +0100

    Filter Tasks implemented

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,2 +1,12 @@
-export const filterTodos = (filter, todos) =>
-  filter ? todos.filter((todo) => todo.isComplete === (filter === "completed")) : todos;
+export const filterTodos = (filter, todos) => {
+  console.log(filter);
+  console.log(todos);
+  if(filter === 'active') {
+    return todos.filter((todo) => todo.isComplete == 0);
+  } else if (filter === 'completed') {
+    return todos.filter((todo) => todo.isComplete == 1);
+  } else {
+    return todos;
+  }
+  // return filter ? todos.filter((todo) => todo.isComplete === (filter === "completed")) : todos;
+}
